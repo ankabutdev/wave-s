448e1698fb52713b3ef5ad2981f7730415e9dc0c
ankabutdev <Ankabut571@gmail.com>
2024-01-10 18:59:33 +0500

    add dtos

---
+++ src/ProductService/ProductService.Application/DTOs/Categories/CategoryUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductService.Application.DTOs.Categories;
+
+public class CategoryUpdateDto
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+}
