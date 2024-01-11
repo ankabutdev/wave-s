f238ef62f665def2014a4d11ec7f06acd7311b06
ankabutdev <Ankabut571@gmail.com>
2024-01-12 00:34:44 +0500

    added method in product

---
+++ src/ProductService/ProductService.Application/UseCases/Products/Queries/GetByIdProduct/GetByIdProductQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ProductService.Domain.Entities;
+
+namespace ProductService.Application.UseCases.Products.Queries.GetByIdProduct;
+
+public class GetByIdProductQuery : IRequest<Product>
+{
+    public int Id { get; set; }
+}
