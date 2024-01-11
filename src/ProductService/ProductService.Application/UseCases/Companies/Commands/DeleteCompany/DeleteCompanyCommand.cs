1a0c256d7c918579bc486d2701a65237a0b6b52f
ankabutdev <Ankabut571@gmail.com>
2024-01-12 00:32:28 +0500

    improve

---
+++ src/ProductService/ProductService.Application/UseCases/Companies/Commands/DeleteCompany/DeleteCompanyCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace ProductService.Application.UseCases.Companies.Commands.DeleteCompany;
+
+public class DeleteCompanyCommand : IRequest<bool>
+{
+    public int Id { get; set; }
+}
