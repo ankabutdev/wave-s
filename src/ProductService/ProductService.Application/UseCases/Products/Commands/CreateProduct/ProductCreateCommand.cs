5ad39c7a1d2e9cc8aae4655217551b277653c842
ankabutdev <Ankabut571@gmail.com>
2024-01-12 00:33:23 +0500

    improve

---
+++ src/ProductService/ProductService.Application/UseCases/Products/Commands/CreateProduct/ProductCreateCommand.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+
+namespace ProductService.Application.UseCases.Products.Commands.CreateProduct;
+
+public class ProductCreateCommand : IRequest<bool>
+{
+    public int CategoryId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string ImagePaths { get; set; } = null!;
+
+    public double Price { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    public long CompanyId { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public string Frame { get; set; } = null!;
+
+    public string Mounted { get; set; } = null!;
+
+    public string Screen { get; set; } = null!;
+
+    public string Buttons { get; set; } = null!;
+
+    public double Weight { get; set; }
+
+    public string Backlight { get; set; } = null!;
+
+    public string Type { get; set; } = null!;
+
+    public string Foam { get; set; } = null!;
+
+    public string Mum { get; set; } = null!;
+
+    public string Smartpause { get; set; } = null!;
+
+    public string Turbopressure { get; set; } = null!;
+}
---
+++ src/ProductService/ProductService.Application/UseCases/Products/Commands/CreateProduct/ProductCreateCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using MediatR;
+using ProductService.Application.Abstractions;
+using ProductService.Domain.Entities;
+
+namespace ProductService.Application.UseCases.Products.Commands.CreateProduct;
+
+public class ProductCreateCommandHandler : IRequestHandler<ProductCreateCommand, bool>
+{
+    private readonly IAppDbContext _context;
+    private readonly IMapper _mapper;
+
+    public ProductCreateCommandHandler(IAppDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<bool> Handle(ProductCreateCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var entity = _mapper.Map<Product>(request);
+
+            entity.CreatedAt = DateTime.UtcNow;
+            entity.UpdatedAt = DateTime.UtcNow;
+
+            await _context.Products.AddAsync(entity, cancellationToken);
+
+            var result = await _context
+                .SaveChangesAsync(cancellationToken);
+
+            return result > 0;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
