e7f988dcf0756e291723b15fcadcabf8ea8ca393
ankabutdev <Ankabut571@gmail.com>
2024-01-12 00:31:27 +0500

    add company entity

---
+++ src/ProductService/ProductService.Application/UseCases/Categories/Commands/DeleteCategory/CategoryDeleteCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ProductService.Application.Abstractions;
+
+namespace ProductService.Application.UseCases.Categories.Commands.DeleteCategory;
+
+public class CategoryDeleteCommandHandler : IRequestHandler<CategoryDeleteCommand, bool>
+{
+    private readonly IAppDbContext _context;
+    private readonly IMapper _mapper;
+
+    public CategoryDeleteCommandHandler(IAppDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<bool> Handle(CategoryDeleteCommand request, CancellationToken cancellationToken)
+    {
+        if (request.Id <= 0)
+            return false;
+
+        var categories = await _context.Categories
+            .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+        if (categories == null)
+            return false;
+
+        _context.Categories.Remove(categories);
+
+        var result = await _context
+            .SaveChangesAsync(cancellationToken);
+
+        return result > 0;
+    }
+}
