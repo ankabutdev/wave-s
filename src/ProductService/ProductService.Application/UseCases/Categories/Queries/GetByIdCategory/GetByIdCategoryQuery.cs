df89b53dfff30db4594874b16686f2155711c8bc
ankabutdev <Ankabut571@gmail.com>
2024-01-12 00:31:58 +0500

    add category entity

---
+++ src/ProductService/ProductService.Application/UseCases/Categories/Queries/GetByIdCategory/GetByIdCategoryQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ProductService.Domain.Entities;
+
+namespace ProductService.Application.UseCases.Categories.Queries.GetByIdCategory;
+
+public class GetByIdCategoryQuery : IRequest<Category>
+{
+    public int Id { get; set; }
+}
