2ef613a26bf763985f8759760b07d2ebe71b0b12
ankabutdev <Ankabut571@gmail.com>
2024-01-11 22:02:08 +0500

    added dto

---
+++ src/ProductService/ProductService.Application/DTOs/Companies/CompanyCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ProductService.Application.DTOs.Companies;
+
+public class CompanyCreateDto
+{
+    [JsonIgnore]
+    public int Id { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string CompanyPhoneNumber { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+}
---
+++ src/ProductService/ProductService.Application/DTOs/Companies/CompanyUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ProductService.Application.DTOs.Companies;
+
+public class CompanyUpdateDto
+{
+    [JsonIgnore]
+    public long Id { get; set; }
+
+    public string Description { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+
+    public string CompanyPhoneNumber { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+}
