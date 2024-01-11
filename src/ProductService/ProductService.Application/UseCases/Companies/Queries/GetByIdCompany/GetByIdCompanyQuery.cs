9a25d1f4d4f5b1b9b3225e066e8fb05575348ffd
ankabutdev <Ankabut571@gmail.com>
2024-01-12 00:32:53 +0500

    improve

---
+++ src/ProductService/ProductService.Application/UseCases/Companies/Queries/GetByIdCompany/GetByIdCompanyQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using ProductService.Domain.Entities;
+
+namespace ProductService.Application.UseCases.Companies.Queries.GetByIdCompany;
+
+public class GetByIdCompanyQuery : IRequest<Company>
+{
+    public int Id { get; set; }
+}
+
---
+++ src/ProductService/ProductService.Application/UseCases/Companies/Queries/GetByIdCompany/GetByIdCompanyQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ProductService.Application.Abstractions;
+using ProductService.Domain.Entities;
+
+namespace ProductService.Application.UseCases.Companies.Queries.GetByIdCompany;
+
+public class GetByIdCompanyQueryHandler : IRequestHandler<GetByIdCompanyQuery, Company>
+{
+    private readonly IAppDbContext _context;
+    
+    public GetByIdCompanyQueryHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Company> Handle(GetByIdCompanyQuery request, CancellationToken cancellationToken)
+    {
+        var result = await _context.Companies
+            .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+        return result ?? throw new Exception("Company not found!");
+    }
+}
