67f21af076fcb99155a930878a5ac06038de0b25
ankabutdev <Ankabut571@gmail.com>
2024-01-13 19:31:18 +0500

    added getAll order

---
+++ src/OrderService/OrderService.Application/UseCases/Orders/Queries/GetAllOrder/GetAllOrderQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using OrderService.Domain.Entities;
+
+namespace OrderService.Application.UseCases.Orders.Queries.GetAllOrder;
+
+public class GetAllOrderQuery : IRequest<IEnumerable<Order>>
+{
+}
