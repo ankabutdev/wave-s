ac1fd539f8699dd5b6f30b501ed2cccde75f129d
ankabutdev <Ankabut571@gmail.com>
2024-01-13 19:33:46 +0500

    add getBy Id user

---
+++ src/UserService/UserService.Application/UseCases/Users/Queries/GetByIdUser/GetByIdUserQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using UserService.Domain.Entities.Users;
+
+namespace UserService.Application.UseCases.Users.Queries.GetByIdUser;
+
+public class GetByIdUserQuery : IRequest<User>
+{
+    public int Id { get; set; }
+}
