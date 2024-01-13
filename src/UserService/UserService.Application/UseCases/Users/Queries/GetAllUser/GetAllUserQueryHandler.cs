a7a746e213625f2abd2529bcb83a37cf67af3298
ankabutdev <Ankabut571@gmail.com>
2024-01-13 19:33:25 +0500

    add get all user

---
+++ src/UserService/UserService.Application/UseCases/Users/Queries/GetAllUser/GetAllUserQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using UserService.Application.Abstractions;
+using UserService.Domain.Entities.Users;
+
+namespace UserService.Application.UseCases.Users.Queries.GetAllUser;
+
+public class GetAllUserQueryHandler : IRequestHandler<GetAllUserQuery, IEnumerable<User>>
+{
+    private readonly IAppDbContext _context;
+
+    public GetAllUserQueryHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<User>> Handle(GetAllUserQuery request, CancellationToken cancellationToken)
+    {
+        return await _context
+            .Users
+            //.Include(x => x.Products)
+            .ToListAsync(cancellationToken);
+    }
+}
