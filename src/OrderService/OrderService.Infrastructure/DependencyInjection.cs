c5551af90d9d0013d64ca1e622ea70df87ff1317
ankabutdev <Ankabut571@gmail.com>
2024-01-13 16:44:18 +0500

    added persinstence and dep injection

---
+++ src/OrderService/OrderService.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderService.Application.Abstractions;
+using OrderService.Infrastructure.Persistence;
+using System.Text.Json.Serialization;
+
+namespace OrderService.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var dockerConnection = configuration.GetConnectionString("DockerConnection");
+        var defaultConnection = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<IAppDbContext, AppDbContext>(options =>
+        {
+            options.UseNpgsql(defaultConnection);
+        });
+
+        services.AddControllersWithViews()
+            .AddJsonOptions(x => x.JsonSerializerOptions
+            .ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+        return services;
+    }
+}
---
+++ src/OrderService/OrderService.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Application.Abstractions;
+using OrderService.Domain.Entities;
+using System.Reflection;
+
+namespace OrderService.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext, IAppDbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        /*
+         var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+        try
+        {
+            if (databaseCreator is null)
+            {
+                throw new Exception("Database Not Found!");
+            }
+
+            if (!databaseCreator.CanConnect())
+                databaseCreator.CreateAsync();
+
+            if (!databaseCreator.HasTables())
+                databaseCreator.CreateTablesAsync();
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+         */
+    }
+
+    public AppDbContext()
+    {
+
+    }
+
+    async ValueTask<int> IAppDbContext.SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+}
