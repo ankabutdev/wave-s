b095934f717174fdd38cf24249f5b3c4b8d8325d
ankabutdev <Ankabut571@gmail.com>
2024-01-13 19:30:55 +0500

    add delete order

---
+++ src/OrderService/OrderService.Application/UseCases/Orders/Commands/DeleteOrder/OrderDeleteCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace OrderService.Application.UseCases.Orders.Commands.DeleteOrder;
+
+public class OrderDeleteCommand : IRequest<bool>
+{
+    public int Id { get; set; }
+}
