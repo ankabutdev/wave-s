4108004472b626b333345abf50340cd2fbe4deb4
ankabutdev <Ankabut571@gmail.com>
2024-01-13 16:43:47 +0500

    add entity

---
+++ src/OrderService/OrderService.Domain/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿namespace OrderService.Domain.Entities;
+
+public class Order
+{
+    public int Id { get; set; }
+
+    public decimal UserId { get; set; }
+
+    public string Description { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime UpdatedAt { get; set; }
+
+    public int ProductId { get; set; }
+}
