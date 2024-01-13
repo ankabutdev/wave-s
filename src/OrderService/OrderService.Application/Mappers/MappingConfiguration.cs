2be1aa0c8dfc1ca18709ae4589db2c2649f35584
ankabutdev <Ankabut571@gmail.com>
2024-01-13 16:43:01 +0500

    order service added mappers

---
+++ src/OrderService/OrderService.Application/Mappers/MappingConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Application.Mappers
+{
+    internal class MappingConfiguration
+    {
+        public MappingConfiguration()
+        {
+            
+        }
+    }
+}
