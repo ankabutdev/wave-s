882bab308846508784faf820518f19bf3bb6e3fb
ankabutdev <Ankabut571@gmail.com>
2024-01-13 19:33:13 +0500

    add update user

---
+++ src/UserService/UserService.Application/UseCases/Users/Commands/UpdateUser/UserUpdateCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace UserService.Application.UseCases.Orders.Commands.UpdateUser;
+
+public class UserUpdateCommand : IRequest<bool>
+{
+    public int Id { get; set; }
+
+    public string FullName { get; set; } = string.Empty;
+
+    public string PhoneNumber { get; set; } = string.Empty;
+
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ src/UserService/UserService.Application/UseCases/Users/Commands/UpdateUser/UserUpdateCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using UserService.Application.Abstractions;
+
+namespace UserService.Application.UseCases.Orders.Commands.UpdateUser;
+
+public class UserUpdateCommandHandler : IRequestHandler<UserUpdateCommand, bool>
+{
+    private readonly IAppDbContext _context;
+    private readonly IMapper _mapper;
+
+    public UserUpdateCommandHandler(IAppDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<bool> Handle(UserUpdateCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var categories = await _context.Users
+            .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if (categories is null)
+                throw new ArgumentNullException(nameof(categories));
+
+            _mapper.Map(request, categories);
+
+            categories.UpdatedAt = DateTime.UtcNow;
+
+            _context.Users.Update(categories);
+
+            var result = await _context
+                .SaveChangesAsync(cancellationToken);
+
+            return result > 0;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
