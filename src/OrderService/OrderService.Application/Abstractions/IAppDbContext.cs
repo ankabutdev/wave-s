a339d699900da5dbe000bf1096eadbf04bf42bee
ankabutdev <Ankabut571@gmail.com>
2024-01-13 16:42:50 +0500

    order service added abstractions

---
+++ src/OrderService/OrderService.Application/Abstractions/IAppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Domain.Entities;
+
+namespace OrderService.Application.Abstractions;
+
+public interface IAppDbContext
+{
+    public DbSet<Order> Orders { get; set; }
+
+    ValueTask<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
