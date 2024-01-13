d54dd100a6f76d429be8445976b545d6f946866a
ankabutdev <Ankabut571@gmail.com>
2024-01-13 19:30:24 +0500

    added dtos

---
+++ src/OrderService/OrderService.Application/DTOs/Orders/OrderCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderService.Application.DTOs.Orders;
+
+public class OrderCreateDto
+{
+    public int UserId { get; set; }
+
+    public string Description { get; set; } = string.Empty;
+
+    public int ProductId { get; set; }
+}
