ea5e95fa9b70e169de9db47b404c0266c66d6f2b
ankabutdev <Ankabut571@gmail.com>
2024-01-13 19:32:38 +0500

    add abstractions

---
+++ src/UserService/UserService.Application/Abstractions/IAppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Domain.Entities.Users;
+
+namespace UserService.Application.Abstractions;
+
+public interface IAppDbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    ValueTask<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
