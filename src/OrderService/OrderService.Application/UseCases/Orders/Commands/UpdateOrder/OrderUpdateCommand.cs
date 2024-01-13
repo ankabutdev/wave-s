969fe70f5dfab756d10db87f61429c4759fac5bd
ankabutdev <Ankabut571@gmail.com>
2024-01-13 19:31:06 +0500

    add update order

---
+++ src/OrderService/OrderService.Application/UseCases/Orders/Commands/UpdateOrder/OrderUpdateCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace OrderService.Application.UseCases.Orders.Commands.UpdateOrder;
+
+public class OrderUpdateCommand : IRequest<bool>
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public string Description { get; set; } = string.Empty;
+
+    public DateTime UpdatedAt { get; set; }
+
+    public int ProductId { get; set; }
+}
---
+++ src/OrderService/OrderService.Application/UseCases/Orders/Commands/UpdateOrder/OrderUpdateCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using OrderService.Application.Abstractions;
+
+namespace OrderService.Application.UseCases.Orders.Commands.UpdateOrder;
+
+public class OrderUpdateCommandHandler : IRequestHandler<OrderUpdateCommand, bool>
+{
+    private readonly IAppDbContext _context;
+    private readonly IMapper _mapper;
+
+    public OrderUpdateCommandHandler(IAppDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<bool> Handle(OrderUpdateCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var categories = await _context.Orders
+            .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if (categories is null)
+                throw new ArgumentNullException(nameof(categories));
+
+            _mapper.Map(request, categories);
+
+            categories.UpdatedAt = DateTime.UtcNow;
+
+            _context.Orders.Update(categories);
+
+            var result = await _context
+                .SaveChangesAsync(cancellationToken);
+
+            return result > 0;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
