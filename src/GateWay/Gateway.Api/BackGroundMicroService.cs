3e482fac2fed350a2ff993664b0f7e12b555c7b5
ankabutdev <Ankabut571@gmail.com>
2024-01-22 16:31:46 +0500

    added BackroudMicroservice

---
+++ src/GateWay/Gateway.Api/BackGroundMicroService.cs
@@ -0,0 +1,45 @@
+﻿namespace Gateway.Api;
+
+public class BackGroundMicroService : BackgroundService, IHostedService, IHostedLifecycleService
+{
+    private readonly ILogger<BackGroundMicroService> _logger;
+
+    public BackGroundMicroService(ILogger<BackGroundMicroService> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task StartedAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Background service started.");
+        return Task.CompletedTask;
+    }
+
+    public Task StartingAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Background service starting.");
+        return Task.CompletedTask;
+    }
+
+    public Task StoppedAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Background service stopped.");
+        return Task.CompletedTask;
+    }
+
+    public Task StoppingAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Background service stopping.");
+        return Task.CompletedTask;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Background service is executing.");
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+        }
+    }
+}
