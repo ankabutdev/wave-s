67dc6f2bd22223c316b55924a559350a60d9d8a3
ankabutdev <Ankabut571@gmail.com>
2024-01-22 21:52:08 +0500

    added memorycache

--- src/ProductService/ProductService.Api/Controllers/CompaniesController.cs
+++ src/ProductService/ProductService.Api/Controllers/CompaniesController.cs
@@ -1,14 +1,18 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using ProductService.Application.DTOs.Companies;
 using ProductService.Application.UseCases.Companies.Commands.CreateCompany;
 using ProductService.Application.UseCases.Companies.Commands.DeleteCompany;
 using ProductService.Application.UseCases.Companies.Commands.UpdateCompany;
 using ProductService.Application.UseCases.Companies.Queries.GetAllCompany;
 using ProductService.Application.UseCases.Companies.Queries.GetByIdCompany;
+using ProductService.Domain.Entities;
 
 namespace ProductService.Api.Controllers;
+
+#pragma warning disable
 
 [Route("api/companies")]
 [ApiController]
@@ -16,17 +20,37 @@ public class CompaniesController : ControllerBase
 {
     private readonly IMediator _mediator;
     private readonly IMapper _mapper;
+    private readonly IMemoryCache _cache;
 
-    public CompaniesController(IMediator mediator, IMapper mapper)
+    public CompaniesController(IMediator mediator, IMapper mapper,
+        IMemoryCache cache)
     {
         _mediator = mediator;
         _mapper = mapper;
+        _cache = cache;
     }
 
     [HttpGet]
     public async ValueTask<IActionResult> GetAllAsync()
     {
-        return Ok(await _mediator.Send(new GetAllCompanyQuery()));
+        if (_cache.TryGetValue("AllCompanies", out var cachedData))
+        {
+            IEnumerable<Company>? data = (IEnumerable<Company>)cachedData;
+            Console.WriteLine("GET DATA CACHE MEMORY");
+            return Ok(data);
+        }
+
+        var result = await _mediator.Send(new GetAllCompanyQuery());
+
+        var cacheEntryOptions = new MemoryCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1),
+            SlidingExpiration = TimeSpan.FromSeconds(20)
+        };
+
+        _cache.Set("AllCompanies", result, cacheEntryOptions);
+
+        return Ok(result);
     }
 
     [HttpGet("{Id}")]
