4d86590c03ed2ace7897f54bcb740bfd0753ed4f
ankabutdev <Ankabut571@gmail.com>
2024-01-22 21:51:43 +0500

    add memorycache

--- src/OrderService/OrderService.Api/Program.cs
+++ src/OrderService/OrderService.Api/Program.cs
@@ -7,16 +7,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddMemoryCache();
+
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 // app.UseHttpsRedirection();
 app.UseCors(options =>
