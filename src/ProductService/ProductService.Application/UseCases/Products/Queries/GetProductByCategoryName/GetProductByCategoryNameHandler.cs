ec595cfac46efd0178f6b064133aa1fa59c81e09
ankabutdev <Ankabut571@gmail.com>
2024-01-29 21:52:53 +0500

    add getProductQuery

---
+++ src/ProductService/ProductService.Application/UseCases/Products/Queries/GetProductByCategoryName/GetProductByCategoryNameHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using ProductService.Application.Abstractions;
+using ProductService.Domain.Entities;
+
+namespace ProductService.Application.UseCases.Products.Queries.GetProductByCategoryName;
+
+#pragma warning disable
+
+public class GetProductByCategoryNameHandler : IRequestHandler<GetProductByCategoryNameQuery, IQueryable<Product>>
+{
+
+    private readonly IAppDbContext _context;
+
+    public GetProductByCategoryNameHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IQueryable<Product>> Handle(GetProductByCategoryNameQuery request, CancellationToken cancellationToken)
+    {
+        return _context
+            .Products
+            .Where(x => x.Category.Name
+            .Contains(request.CategoryName));
+    }
+}
